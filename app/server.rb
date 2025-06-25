baa0e2db3a2b681a7339ad3a723bd6421d04b4c7
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2025-06-24 20:53:47 -0500

    crear jwt en login con header

--- app/server.rb
+++ app/server.rb
@@ -1,4 +1,8 @@
 require 'sinatra/base'
+require 'json'
+require 'jwt'
+require 'logger'
+require 'dotenv/load'
 
 class ApplicationController < Sinatra::Base
   # Habilitar logging
@@ -13,6 +17,8 @@ class ApplicationController < Sinatra::Base
     set :sessions, expire_after: 3600
     set :public_folder, File.join(Dir.pwd, 'public')
     set :constants, CONSTANTS[:local]
+    set :auth_header, ENV['AUTH_HEADER']
+    set :jwt_secret, ENV['JWT_SECRET']
   end
 
   before do
@@ -24,5 +30,34 @@ class ApplicationController < Sinatra::Base
   get '/' do
     hola()
     'hol'
+  end
+
+  post '/api/v1/sign-in' do
+    incoming_auth = request.env['HTTP_X_AUTH_TRIGGER']
+    # puts "Incoming X-Auth-Trigger: #{incoming_auth}"
+    # puts "Expected AUTH_HEADER: #{settings.auth_header}"
+    if incoming_auth == settings.auth_header
+      puts "Authentication successful. Generating JWT."
+      claims = {
+        iss: 'your-app.com',
+        aud: 'your-client-id',
+        sub: 'user@example.com',
+        exp: Time.now.to_i + 3600, # 1 hora
+        iat: Time.now.to_i,
+        user_id: 123,
+        role: 'admin'
+      }
+      begin
+        token = JWT.encode claims, settings.jwt_secret, 'HS256'
+        { status: 'success', token: token }.to_json
+      rescue => e
+        puts "Failed to encode JWT: #{e.message}"
+        halt 500, { error: 'Failed to generate token' }.to_json
+      end
+    else
+      puts "Unauthorized access attempt. Invalid or missing X-Auth-Trigger."
+      status 401
+      { error: 'Unauthorized', message: 'Invalid or missing X-Auth-Trigger' }.to_json
+    end
   end
 end
